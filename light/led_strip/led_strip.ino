8637659ff6509944b81373bb12102a2a0af24eac
mari__ <bowman.marius@gmail.com>
2021-05-30 18:32:58 +0200

    impl blink demo

--- light/led_strip/led_strip.ino
+++ light/led_strip/led_strip.ino
@@ -1,17 +1,23 @@
 #include <FastLED.h>
 
-
 #define NUM_LEDS 5
 #define LED_PIN D4
 
-CRGB leds [NUM_LEDS]
+CRGB leds [NUM_LEDS];
 
 void setup() {
 //  Serial.begin(115200);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
-//void loop() {
+void loop() {
+  for(int i = 0; i < 5; i++) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(500);
+    leds[i] = CRGB::White;
+  }
+//  
 //  leds[0] = CRGB::Red;
 //  FastLED.show();
 //  delay(500);  
@@ -21,15 +27,4 @@ void setup() {
 //  leds[2] = CRGB::Green;
 //  FastLED.show();
 //  delay(500);
-//}
-
-void loop() { 
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
-  FastLED.show();
-  delay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
 }
