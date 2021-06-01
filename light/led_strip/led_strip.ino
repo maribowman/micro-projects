1c730f0fa5388104de731d37746be193d1f04e6a
mari__ <bowman.marius@gmail.com>
2021-06-01 16:20:31 +0200

    played with led modes

--- light/led_strip/led_strip.ino
+++ light/led_strip/led_strip.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 5
+#define NUM_LEDS 10
 #define LED_PIN D4
 
 CRGB leds [NUM_LEDS];
@@ -10,10 +10,16 @@ void setup() {
 }
 
 void loop() {
-  for(int i = 0; i < 5; i++) {
+  for(int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB::Red;
     FastLED.show();
-    delay(500);
-    leds[i] = CRGB::White;
+    delay(100);
+    leds[i] = CRGB::Black;
+  }
+    for(int i = NUM_LEDS - 1; i >=0; i--) {
+    leds[i] = CRGB::Red;
+    FastLED.show();
+    delay(100);
+    leds[i] = CRGB::Black;
   }
 }
