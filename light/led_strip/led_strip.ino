2b46410b7fe5dad7d1c3bbf0205fc272af04c3ce
mari__ <bowman.marius@gmail.com>
2021-06-02 17:11:46 +0200

    more led stuff

--- light/led_strip/led_strip.ino
+++ light/led_strip/led_strip.ino
@@ -19,47 +19,50 @@ void setup() {
   FastLED.setBrightness(BRIGHTNESS);
 }
 
-//void loop() {
-//  for(int bright = 10; bright <= 255; bright+=5) {
-//    FastLED.setBrightness(bright);
-//    for(int index = 0; index < NUM_LEDS; index++) {
-//      leds[index] = CRGB(hue8, sat8, bri8);;
-//      FastLED.show();
-//    }
-//    delay(100);
-//  }
-//    for(int bright = 255; bright >= 0; bright-=5) {
-//    FastLED.setBrightness(bright);
-//    for(int index = 0; index < NUM_LEDS; index++) {
-//      leds[index] = CRGB();
-//      FastLED.show();
-//    }
-//    delay(100);
-//  }
-//}
-
-void loop()
-{
-  // draw a generic, no-name rainbow
-  static uint8_t starthue = 0;
-  fill_rainbow( leds + 5, NUM_LEDS - 5, --starthue, 20);
-
-  // Choose which 'color temperature' profile to enable.
-  uint8_t secs = (millis() / 1000) % (DISPLAYTIME * 2);
-  if( secs < DISPLAYTIME) {
-    FastLED.setTemperature( TEMPERATURE_1 ); // first temperature
-    leds[0] = TEMPERATURE_1; // show indicator pixel
-  } else {
-    FastLED.setTemperature( TEMPERATURE_2 ); // second temperature
-    leds[0] = TEMPERATURE_2; // show indicator pixel
+void loop() {
+  for(int bright = 10; bright <= 255; bright+=5) {
+    FastLED.setBrightness(bright);
+    for(int index = 0; index < NUM_LEDS; index++) {
+      leds[index] = CHSV(HUE_YELLOW, sat8, bright);;
+      FastLED.show();
+    }
+    delay(100);
   }
 
-  // Black out the LEDs for a few secnds between color changes
-  // to let the eyes and brains adjust
-  if( (secs % DISPLAYTIME) < BLACKTIME) {
-    memset8( leds, 0, NUM_LEDS * sizeof(CRGB));
-  }
+  //CHSV colors (hue, sat, bri) 
   
-  FastLED.show();
-  FastLED.delay(8);
+  for(int bright = 255; bright >= 0; bright-=5) {
+    FastLED.setBrightness(bright);
+    for(int index = 0; index < NUM_LEDS; index++) {
+      leds[index] = CRGB();
+      FastLED.show();
+    }
+    delay(100);
+  }
 }
+
+//void loop()
+//{
+//  // draw a generic, no-name rainbow
+//  static uint8_t starthue = 0;
+//  fill_rainbow( leds + 5, NUM_LEDS - 5, --starthue, 20);
+//
+//  // Choose which 'color temperature' profile to enable.
+//  uint8_t secs = (millis() / 1000) % (DISPLAYTIME * 2);
+//  if( secs < DISPLAYTIME) {
+//    FastLED.setTemperature( TEMPERATURE_1 ); // first temperature
+//    leds[0] = TEMPERATURE_1; // show indicator pixel
+//  } else {
+//    FastLED.setTemperature( TEMPERATURE_2 ); // second temperature
+//    leds[0] = TEMPERATURE_2; // show indicator pixel
+//  }
+//
+//  // Black out the LEDs for a few secnds between color changes
+//  // to let the eyes and brains adjust
+//  if( (secs % DISPLAYTIME) < BLACKTIME) {
+//    memset8( leds, 0, NUM_LEDS * sizeof(CRGB));
+//  }
+//  
+//  FastLED.show();
+//  FastLED.delay(8);
+//}
