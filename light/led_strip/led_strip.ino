b64def1b765d613a09de70d5d1ac4f422ffcec7e
mari__ <bowman.marius@gmail.com>
2021-06-02 17:11:15 +0200

    more led stuff

--- light/led_strip/led_strip.ino
+++ light/led_strip/led_strip.ino
@@ -1,25 +1,65 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 10
 #define LED_PIN D4
+#define NUM_LEDS 50
+#define COLOR_ORDER GRB
+#define LED_TYPE WS2812
+#define BRIGHTNESS 128
 
-CRGB leds [NUM_LEDS];
+#define TEMPERATURE_1 Tungsten40W
+#define TEMPERATURE_2 ClearBlueSky
+#define DISPLAYTIME 5
+#define BLACKTIME 1
+
+struct CRGB leds [NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
-void loop() {
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(100);
-    leds[i] = CRGB::Black;
+//void loop() {
+//  for(int bright = 10; bright <= 255; bright+=5) {
+//    FastLED.setBrightness(bright);
+//    for(int index = 0; index < NUM_LEDS; index++) {
+//      leds[index] = CRGB(hue8, sat8, bri8);;
+//      FastLED.show();
+//    }
+//    delay(100);
+//  }
+//    for(int bright = 255; bright >= 0; bright-=5) {
+//    FastLED.setBrightness(bright);
+//    for(int index = 0; index < NUM_LEDS; index++) {
+//      leds[index] = CRGB();
+//      FastLED.show();
+//    }
+//    delay(100);
+//  }
+//}
+
+void loop()
+{
+  // draw a generic, no-name rainbow
+  static uint8_t starthue = 0;
+  fill_rainbow( leds + 5, NUM_LEDS - 5, --starthue, 20);
+
+  // Choose which 'color temperature' profile to enable.
+  uint8_t secs = (millis() / 1000) % (DISPLAYTIME * 2);
+  if( secs < DISPLAYTIME) {
+    FastLED.setTemperature( TEMPERATURE_1 ); // first temperature
+    leds[0] = TEMPERATURE_1; // show indicator pixel
+  } else {
+    FastLED.setTemperature( TEMPERATURE_2 ); // second temperature
+    leds[0] = TEMPERATURE_2; // show indicator pixel
   }
-    for(int i = NUM_LEDS - 1; i >=0; i--) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(100);
-    leds[i] = CRGB::Black;
+
+  // Black out the LEDs for a few secnds between color changes
+  // to let the eyes and brains adjust
+  if( (secs % DISPLAYTIME) < BLACKTIME) {
+    memset8( leds, 0, NUM_LEDS * sizeof(CRGB));
   }
+  
+  FastLED.show();
+  FastLED.delay(8);
 }
